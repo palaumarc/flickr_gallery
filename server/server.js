4a0235c10f4abd3a4254f7b99b310edba1f37bb3
palaumarc <marcpalau91@gmail.com>
2018-09-29 21:02:36 +0200

    Grab square photo url from flickr API as previewUrl

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use('/', express.static('../client/build'));
 
 app.get('/api/photos', (req, res) => {
 
-    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${config.apiKey}&extras=url_l%2C+url_s%2C+description%2C+media%2C+owner_name&format=json&nojsoncallback=1`;
+    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${config.apiKey}&extras=url_l%2C+url_q%2C+description%2C+media%2C+owner_name&format=json&nojsoncallback=1`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -16,7 +16,7 @@ app.get('/api/photos', (req, res) => {
                 return {
                     id: photo.id,
                     fullSizeUrl: photo.url_l,
-                    previewUrl: photo.url_s,
+                    previewUrl: photo.url_q,
                     flickrRedirectUrl: `https://www.flickr.com/photos/${photo.owner}/${photo.id}`,
                     title: photo.title,
                     description: photo.description._content,
