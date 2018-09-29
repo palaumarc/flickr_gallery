07760d10e78e4a89ce2d5363f65f35abd66546b0
palaumarc <marcpalau91@gmail.com>
2018-09-29 18:03:56 +0200

    Update /api/photos endpoint to get photos from Flickr API

--- server/server.js
+++ server/server.js
@@ -1,39 +1,30 @@
 const express = require('express');
+const fetch = require('node-fetch');
+const config = require('./config')
 const app = express();
 const port = 8080;
 
-const photosMock = [
-    {
-        id: 12,
-        fullSizeUrl: 'https://dummyimage.com/600x400/000/fff',
-        previewUrl: 'https://dummyimage.com/600x400/000/fff',
-        flickrRedirectUrl: 'https://google.com',
-        title: 'MockPhoto',
-        description: 'This is a great mock photo',
-        username: 'Woody'
-    },
-    {
-        id: 13,
-        fullSizeUrl: 'https://dummyimage.com/300x400/000/fff',
-        previewUrl: 'https://dummyimage.com/300x400/000/fff',
-        flickrRedirectUrl: 'https://google.com',
-        title: 'MockPhoto2',
-        description: 'This is a great second mock photo',
-        username: 'Buzz'
-    },
-    {
-        id: 14,
-        fullSizeUrl: 'https://dummyimage.com/300x200/000/fff',
-        previewUrl: 'https://dummyimage.com/300x200/000/fff',
-        flickrRedirectUrl: 'https://google.com',
-        title: 'MockPhoto3',
-        description: 'This is a great third mock photo',
-        username: 'Jessie'
-    }
-]
-
 app.use('/', express.static('../client/build'));
 
-app.get('/api/photos', (req, res) => res.status(200).json(photosMock));
+app.get('/api/photos', (req, res) => {
+
+    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${config.apiKey}&extras=url_l%2C+url_s%2C+description%2C+media%2C+owner_name&format=json&nojsoncallback=1`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const photos = data.photos.photo.map(photo => {
+                return {
+                    id: photo.id,
+                    fullSizeUrl: photo.url_l,
+                    previewUrl: photo.url_s,
+                    flickrRedirectUrl: `https://www.flickr.com/photos/${photo.owner}/${photo.id}`,
+                    title: photo.title,
+                    description: photo.description._content,
+                    username: photo.ownername
+                }
+            })
+            res.status(200).json(photos);
+        })
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
