3a056ae96ab82aa7022da13b3dfd21ed8147f4fa
palaumarc <marcpalau91@gmail.com>
2018-09-30 12:52:03 +0200

    Add additional info of the photo in the lightbox.

--- client/src/components/PhotoDetail.js
+++ client/src/components/PhotoDetail.js
@@ -6,6 +6,10 @@ const PhotoDetail = ({ photo, onClose }) => (
         <div className="photo-detail-close" onClick={onClose}>&times;</div>
         <div className="photo-detail-content">
             <img src={photo.fullSizeUrl || photo.previewUrl} alt={photo.title}/>
+            <div>
+                <span>{photo.title}</span> - <span>{photo.username}</span>
+                <p>{photo.description}</p>
+            </div>
         </div>
     </div>
 );
