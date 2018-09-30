478344540c86f1fcf222b8701df7545b1be79cc1
palaumarc <marcpalau91@gmail.com>
2018-09-30 19:56:47 +0200

    Final styles to PhotoDetail

--- client/src/components/PhotoDetail.js
+++ client/src/components/PhotoDetail.js
@@ -3,16 +3,16 @@ import './PhotoDetail.css'
 
 const PhotoDetail = ({ photo, onClose, onClickNext, onClickPrevious, hasNext, hasPrevious }) => (
     <div className="photo-detail">
-        <div className="photo-detail-close" onClick={onClose}>&times;</div>
         <div className="photo-detail-content">
+            <div className="photo-detail-close" onClick={onClose}>&times;</div>
             <img src={photo.fullSizeUrl || photo.previewUrl} alt={photo.title} />
-            <div className="photo-detail-next-previous-container">
-                {hasPrevious ? <div className="photo-detail-previous" onClick={onClickPrevious}>&lt;</div> : null}
-                {hasNext ? <div className="photo-detail-next" onClick={onClickNext}>&gt;</div> : null}
-            </div>
-            <div>
-                <span>{photo.title}</span> - <span className="photo-detail-content-username" onClick={() => window.open(photo.flickrRedirectUrl)}>{photo.username}</span>
+            <div className="photo-detail-content-info-container">
+                <span className="photo-detail-content-info-title">{photo.title}</span> - <span className="photo-detail-content-info-username" onClick={() => window.open(photo.flickrRedirectUrl)}>{photo.username}</span>
                 <p>{photo.description}</p>
+            </div>
+            <div className="photo-detail-content-next-previous-container">
+                {hasPrevious ? <div className="photo-detail-content-previous" onClick={onClickPrevious}>&lt;</div> : null}
+                {hasNext ? <div className="photo-detail-content-next" onClick={onClickNext}>&gt;</div> : null}
             </div>
         </div>
     </div>
