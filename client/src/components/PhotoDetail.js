400bc25aaf56ffff01cf49ec3afde2a7741a352e
palaumarc <marcpalau91@gmail.com>
2018-10-01 18:37:43 +0200

    Fix heigth bug of photo detail

--- client/src/components/PhotoDetail.js
+++ client/src/components/PhotoDetail.js
@@ -5,8 +5,11 @@ const PhotoDetail = ({ photo }) => (
     <div className="photo-detail-content">
         <img src={photo.fullSizeUrl || photo.previewUrl} alt={photo.title} />
         <div className="photo-detail-content-info-container">
-            <span className="photo-detail-content-info-title">{photo.title}</span> - <span className="photo-detail-content-info-username" onClick={() => window.open(photo.flickrRedirectUrl)}>{photo.username}</span>
-            <p>{photo.description}</p>
+            <span className="photo-detail-content-info-title">{photo.title}</span> 
+            - 
+            <span className="photo-detail-content-info-username" onClick={() => window.open(photo.flickrRedirectUrl)}>{photo.username}</span>
+            <br />
+            <span className="photo-detail-content-info-description">{photo.description}</span>
         </div>
     </div>
 );
