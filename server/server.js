380bf05804df0846a97af3d70a9abdd4bfc02a8b
palaumarc <marcpalau91@gmail.com>
2018-10-02 08:43:51 +0200

    Extract Express app generation to controller/rest

--- server/server.js
+++ server/server.js
@@ -1,43 +1,5 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const config = require('./config')
-const app = express();
+const app = require('./controller/rest');
+
 const port = 8080;
 
-app.use('/', express.static('../client/build'));
-
-app.get('/api/photos', (req, res) => {
-
-    const perPage = req.query.per_page;
-    const page = req.query.page;
-
-    let url = `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${config.apiKey}&extras=url_l%2C+url_q%2C+description%2C+media%2C+owner_name&format=json&nojsoncallback=1`;
-    
-    if (perPage) {
-        url += `&per_page=${perPage}`;
-    }
-
-    if (page) {
-        url += `&page=${page}`;
-    }
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const photos = data.photos.photo.map(photo => {
-                return {
-                    id: photo.id,
-                    fullSizeUrl: photo.url_l,
-                    previewUrl: photo.url_q,
-                    flickrRedirectUrl: `https://www.flickr.com/photos/${photo.owner}/${photo.id}`,
-                    title: photo.title,
-                    description: photo.description._content,
-                    username: photo.ownername
-                }
-            })
-            const hasMore = data.photos.page < data.photos.pages;
-            res.status(200).json({photos, hasMore});
-        })
-});
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
